bf3c8666e58ea2e55cb4abf17268c1afd251b9f8
stevenhuyn <stevenhuyn@gmail.com>
2023-08-26 12:11:24 -0700

    Disable spymaster role atm

--- src/pages/RolePage.tsx
+++ src/pages/RolePage.tsx
@@ -6,7 +6,7 @@ export const RolePage = () => {
       <h1 class="font-light text-4xl m-6">Welcome to Donkey Glue</h1>
       <h2 class="font-light text-2xl m-8">Choose your Role</h2>
       <div>
-        <A role="button" class="btn mx-3" href="/game?role=RedSpymaster">
+        <A role="button" class="btn btn-disabled mx-3" href="/game?role=RedSpymaster">
           Spymaster
         </A>
         <A role="button" class="btn mx-3" href="/game?role=RedOperative">

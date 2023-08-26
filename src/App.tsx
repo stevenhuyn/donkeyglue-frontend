392feaf7f34fecb00bcea18ec67e56b23ff20ced
stevenhuyn <stevenhuyn@gmail.com>
2023-08-26 03:04:37 -0700

    Add 404 Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Route, Routes } from "@solidjs/router";
 import { GamePage } from "./pages/GamePage";
 import { RolePage } from "./pages/RolePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" component={RolePage}></Route>
       <Route path="/game" component={GamePage}></Route>
+      <Route path="/*" component={NotFoundPage}></Route>
     </Routes>
   );
 };
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,3 @@
+export const NotFoundPage = () => {
+  return <h1 class="font-light text-4xl m-6">404 Not Found</h1>;
+};

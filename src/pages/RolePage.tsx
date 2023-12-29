81c0cea1c175103d5fdf1c49d79f54c00992b266
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2023-12-28 19:51:01 -0800

    Make buttons flex

--- src/pages/RolePage.tsx
+++ src/pages/RolePage.tsx
@@ -5,11 +5,11 @@ export const RolePage = () => {
     <>
       <h1 class="font-light text-4xl m-6">Welcome to Donkey Glue</h1>
       <h2 class="font-light text-2xl m-8">Choose your Role</h2>
-      <div>
-        <A role="button" class="btn mx-3" href="/game?role=RedSpymaster">
+      <div class="flex justify-center space-x-4">
+        <A role="button" class="btn" href="/game?role=RedSpymaster">
           Red Spymaster
         </A>
-        <A role="button" class="btn mx-3" href="/game?role=RedOperative">
+        <A role="button" class="btn" href="/game?role=RedOperative">
           Red Operative
         </A>
       </div>
